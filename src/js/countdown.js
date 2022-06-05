4d95e141e1b29c4041d8c13e8db84cb3bc2e0280
vitorss <vitorss@gmail.com>
2022-06-05 15:56:38 -0300

    New design

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,22 +1,21 @@
-const count = new Date("august 09, 2021, 18:00:00").getTime();
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var d = count - now;
+const count = new Date("july 04, 2022, 18:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var d = count - now;
 
-    var days = Math.floor(d/(1000*60*60*24));
-    var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
-    var minutes = Math.floor((d%(1000*60*60))/(1000*60));
-    var seconds = Math.floor((d%(1000*60))/1000);
+  var days = Math.floor(d / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((d % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
 
-    if(d <= 0) {
-        clearInterval(x);
-    } 
+  if (d <= 0) {
+    clearInterval(x);
+  }
 
-    d = (d <= 0) ? clearInterval(x) : false; 
-
+  d = d <= 0 ? clearInterval(x) : false;
 }, 1000);
